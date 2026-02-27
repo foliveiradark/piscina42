1dd2075746b6ec492035a28bb744e72d66c15491
Alexia Cristina Passos Santos <alecrist@c2r8p2.42sp.org.br>
2026-02-27 18:33:23 -0300

    mandando coisas

---
+++ rush_0.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putchar(char abobra)
+{
+    write(1, &abobra, 1);
+}
+
+void rush(int largura, int altura)
+{
+    int n = 0;
+    int quebra_de_linha = largura + 1;
+    int total = quebra_de_linha * altura;
+
+    while(n < total)
+    {
+        //CANTOS
+        if(n <= largura && n % quebra_de_linha == 1)
+            ft_putchar('O');
+        else if (n <= largura && n % quebra_de_linha == largura)
+            ft_putchar('O');
+        else if (n >= total - quebra_de_linha && n % quebra_de_linha == 1)
+            ft_putchar('O');
+        else if(n >= total - quebra_de_linha && n % quebra_de_linha == largura)
+            ft_putchar('O');
+
+        //QUEBRA DE LINHA
+        else if(n % quebra_de_linha == 0)
+            ft_putchar('\n');
+
+        //PAREDES
+        else if (n % quebra_de_linha == 1)
+            ft_putchar('|');
+        else if (n % quebra_de_linha == largura)
+            ft_putchar('|');
+
+        //CHAO E TETO
+        else if (n <= largura)
+            ft_putchar('-');
+        else if (n >= total - quebra_de_linha)
+            ft_putchar('-');
+
+        //medo eterno de todas as verdades
+        else
+            ft_putchar(' ');
+        n++;
+    }
+}
+
+int main(void)
+{
+    rush(1, 5);
+}
