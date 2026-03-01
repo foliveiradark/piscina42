3342ed0a05010629f0aed54c6b70cc26b831ef6e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-03-01 16:52:14 -0300

    muda variaveis para ingles no arquivo rush01.c

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -6,44 +6,44 @@
 /*   By: dode-lim <dode-lim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 15:37:47 by hvaini-d          #+#    #+#             */
-/*   Updated: 2026/03/01 16:48:39 by dode-lim         ###   ########.fr       */
+/*   Updated: 2026/03/01 16:51:42 by dode-lim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char letter);
 
-int	print_sides(int n, int x, int y, int quebra_de_linha)
+int	print_sides(int n, int x, int y, int break_line)
 {
 	int	total;
 
-	total = y * quebra_de_linha;
-	if (n <= x && n % quebra_de_linha == 1)
+	total = y * break_line;
+	if (n <= x && n % break_line == 1)
 		ft_putchar('/');
-	else if (n <= x && n % quebra_de_linha == x)
+	else if (n <= x && n % break_line == x)
 		ft_putchar('\\');
-	else if (n >= total - quebra_de_linha && n % quebra_de_linha == 1)
+	else if (n >= total - break_line && n % break_line == 1)
 		ft_putchar('\\');
-	else if (n >= total - quebra_de_linha && n % quebra_de_linha == x)
+	else if (n >= total - break_line && n % break_line == x)
 		ft_putchar('/');
 	else
 		return (0);
 	return (1);
 }
 
-int	print_body(int n, int x, int y, int quebra_de_linha)
+int	print_body(int n, int x, int y, int break_line)
 {
 	int	total;
 
-	total = y * quebra_de_linha;
-	if (n % quebra_de_linha == 0)
+	total = y * break_line;
+	if (n % break_line == 0)
 		ft_putchar('\n');
-	else if (n % quebra_de_linha == 1)
+	else if (n % break_line == 1)
 		ft_putchar('*');
-	else if (n % quebra_de_linha == x)
+	else if (n % break_line == x)
 		ft_putchar('*');
 	else if (n <= x)
 		ft_putchar('*');
-	else if (n >= total - quebra_de_linha)
+	else if (n >= total - break_line)
 		ft_putchar('*');
 	else
 		ft_putchar(' ');
@@ -79,7 +79,7 @@ void	trigger_error(void)
 void	rush(int x, int y)
 {
 	int	n;
-	int	quebra_de_linha;
+	int	break_line;
 	int	total;
 	int	has_printed;
 
@@ -89,13 +89,13 @@ void	rush(int x, int y)
 		return ;
 	}
 	n = 1;
-	quebra_de_linha = x + 1;
-	total = quebra_de_linha * y;
+	break_line = x + 1;
+	total = break_line * y;
 	has_printed = 0;
 	while (n < total)
 	{
-		if (print_sides(n, x, y, quebra_de_linha) == 0)
-			print_body(n, x, y, quebra_de_linha);
+		if (print_sides(n, x, y, break_line) == 0)
+			print_body(n, x, y, break_line);
 		n++;
 	}
 	ft_putchar('\n');
