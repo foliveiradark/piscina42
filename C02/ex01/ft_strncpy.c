6ba325882e4737dc07254965cff4ec91930d9122
Flávio Oliveira <foliveiradark@gmail.com>
2026-03-15 10:28:00 -0300

    Adiciona ex01 da lista C02

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flavio <flavio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/15 09:54:11 by flavio            #+#    #+#             */
+/*   Updated: 2026/03/15 10:26:17 by flavio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+#include<stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return(dest);
+}
+
+int	main(void)
+{
+	char src[] = "Hello Pisciner";
+	char dest[20] = {0};
+	
+	printf("antes de rodar a função:\n");
+	printf("src = %s\n", src);
+	printf("dest = %s\n", dest);
+	
+	ft_strncpy(dest, src, 5);
+
+	printf("após rodar a função:\n");
+	printf("src = %s\n", src);
+	printf("dest = %s\n", dest);
+}
