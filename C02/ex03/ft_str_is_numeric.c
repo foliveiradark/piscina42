cef86b1dff6f3e46c398d4b3b5d53dfe1f5dc56c
Flavio Campos Oliveira <fcampos-@c2r5p5.42sp.org.br>
2026-03-16 21:22:51 -0300

    Adiciona ex03 e ex04 Lista C02

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcampos- <fcampos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 20:53:06 by fcampos-          #+#    #+#             */
+/*   Updated: 2026/03/16 21:17:06 by fcampos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include<stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+    /* int	main(void)
+    {
+        char str1[] = "123";
+        char str2[] = "Piscina42";
+        char str3[] = "";
+        
+        printf("%s - somente Números = %d\n", str1, ft_str_is_numeric(str1));
+        printf("%s - letras e números = %d\n", str2, ft_str_is_numeric(str2));
+        printf("%s - string vazia = %d\n", str3, ft_str_is_numeric(str3));
+
+        return(0);
+    } */
