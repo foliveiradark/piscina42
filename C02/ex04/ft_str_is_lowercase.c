798bd3b990b4e9733580bb922af35e189fe99b33
Flávio Oliveira <foliveiradark@gmail.com>
2026-03-18 09:37:39 -0300

    Corrige código ex04 Lista C02 que não foi carregado corretamente

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -3,22 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcampos- <fcampos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: flavio <flavio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 21:14:06 by fcampos-          #+#    #+#             */
-/*   Updated: 2026/03/16 21:18:30 by fcampos-         ###   ########.fr       */
+/*   Updated: 2026/03/18 09:34:56 by flavio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
+#include <stdio.h>
 
-int	ft_str_is_lowercase(char *char)
+int	ft_str_is_lowercase(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (!str)
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			return (0);
+		}
+		i++;
 	}
+	return (1);
+}
+
+int	main(void)
+{
+	char	str1[] = "1234";
+	char	str2[] = "piscina";
+	char	str3[] = "PISCINA";
+	char	str4[] = "";
+
+	printf("%s - somente Números = %d\n", str1, ft_str_is_lowercase(str1));
+	printf("%s - somente minúsculas = %d\n", str2, ft_str_is_lowercase(str2));
+	printf("%s - somente maiusculas = %d\n", str3, ft_str_is_lowercase(str3));
+	printf("%s - string vazia = %d\n", str4, ft_str_is_lowercase(str4));
 }
