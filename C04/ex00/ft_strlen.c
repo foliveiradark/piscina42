1289975d83f84711cbb6647e87fff62f086925f7
Flávio Oliveira <foliveiradark@gmail.com>
2026-03-24 12:35:53 -0300

    Adiciona ex00 ft_strlen Lista C04

---
+++ C04/ex00/ft_strlen.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flavio <flavio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/24 10:30:31 by flavio            #+#    #+#             */
+/*   Updated: 2026/03/24 12:34:55 by flavio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/* void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(123);
+	ft_putchar('\n');
+	ft_putstr("teste 42");
+	ft_putchar('\n');
+	ft_putnbr(ft_strlen("123   *++--++"));
+	ft_putchar('\n');
+} */
