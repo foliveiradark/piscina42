fe0f674cca9d636fb565573ed2ef72c1f26c2012
Flávio Oliveira <foliveiradark@gmail.com>
2026-03-29 12:32:50 -0300

    Adiciona ex04 ft_putnbr_base Lista C04

---
+++ C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flavio <flavio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/29 11:32:44 by flavio            #+#    #+#             */
+/*   Updated: 2026/03/29 12:30:48 by flavio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!base || base[0] == '\0' || base[1] == '\0')
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	long	nb;
+	int		base_len;
+
+	base_len = ft_check_base(base);
+	if (base_len == 0)
+		return ;
+	nb = nbr;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= base_len)
+		ft_putnbr_base(nb / base_len, base);
+	ft_putchar(base[nb % base_len]);
+}
+
+int	main(void)
+{
+	ft_putnbr_base(90, "0123456789ABCDEF");
+	ft_putchar('\n');
+}
