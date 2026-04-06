61b2b5ca658697d77e3d902eac1ce16b6ab69ad9
Flávio Oliveira <foliveiradark@gmail.com>
2026-04-06 14:07:17 -0300

    Adiciona ex00 ft_iterative_factorial Lista C05

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flavio <flavio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/06 13:21:37 by flavio            #+#    #+#             */
+/*   Updated: 2026/04/06 14:01:02 by flavio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	result = 1;
+	while (nb > 0)
+	{
+		result = result * nb;
+		nb--;
+	}
+	return (result);
+}
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');	
+}
+
+int	main (void)
+{
+	ft_putnbr(ft_iterative_factorial(4));
+	ft_putchar('\n');
+}
